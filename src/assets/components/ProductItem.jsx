7d57074885246f67c9e3857a01a501a86f646720
Chris213-boop <46472499@fi.unju.edu.ar>
2025-05-11 18:48:54 -0300

    Incorporo las propiedades del objeto Producto en ProductItem Torres Cintia Cristina

--- src/assets/components/ProductItem.jsx
+++ src/assets/components/ProductItem.jsx
@@ -1,1 +1,19 @@
 //para representar cada producto individual.
+
+
+let idCounter = 0;
+
+function Item (descripcion, precioUnitario, descuento, precioConDescuento, stock, fechaIncorporacion ){
+    //Nuevo Producto
+    return {
+        id: ++idCounter,  //id unico
+        descripcion,
+        precioUnitario,
+        descuento,
+        precioConDescuento,
+        stock,
+        fechaIncorporacion
+    };
+}
+
+export default Item;
