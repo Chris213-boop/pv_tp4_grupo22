d6bbf39620430fb432704e9b3ec0da41e1c32db2
maxi00002 <maxi00002>
2025-05-12 11:45:30 -0300

    incorporacion de la barra de busqueda AntenorMaximiliano

--- src/assets/components/App.jsx
+++ src/assets/components/App.jsx
@@ -2,10 +2,13 @@ import { useState, useEffect, useCallback } from 'react'
 import ProductForm from './ProductForm'
 import '../css/Estilos.css'
 import ProductDelete from './ProductDelete'
+import SearchBar from './SearchBar';
 import ProductEdit from './ProductEdit';
 
  function App() {
   const [productos, setProductos] = useState([]);
+  const [resultados, setResultados] = useState([]);
+
 
   useEffect(() => {
     if (productos.length > 0) {
@@ -30,12 +33,35 @@ import ProductEdit from './ProductEdit';
       )
     );
   }, []);
+  const buscarProducto = useCallback((termino) => {
+  const resultado = productos.filter(producto => {
+    const idMatch = producto.id.toString() === termino;
+    const descMatch = producto.descripcion.toLowerCase().includes(termino.toLowerCase());
+    return idMatch || descMatch;
+  });
+  setResultados(resultado);
+}, [productos]);
+
  
    return (
      <> 
       <main className='container'>
           <ProductForm onAddProduct={handleAgregarProducto} />
           <aside className='aside'>
+          <SearchBar onSearch={buscarProducto} />
+          {resultados.length > 0 && (
+          <section>
+          <h3 className="titulo">Resultados de Búsqueda</h3>
+        <ul>
+          {resultados.map(producto => (
+            <li key={producto.id}>
+          {producto.descripcion} - Precio: ${producto.precioUnitario} - Descuento: {producto.descuento}% - Precio Final: ${producto.precioConDescuento.toFixed(2)} - Stock: {producto.stock}
+          </li>
+       ))}
+      </ul>
+    </section>
+)}
+
             <h2 className='titulo'>Lista de Productos Agregados</h2>
             {productos.length === 0 ? (
               <p>Aún no hay productos. ¡Agrega uno!</p>
