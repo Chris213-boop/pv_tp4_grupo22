26fd217b19f16cb507becfb8327df0202878f700
PedroDardoRamonCayo <pedrodardoramoncayo@gmail.com>
2025-05-19 09:46:22 -0300

    agregado de UseMemo en ambos search, searchBar y SearchResult, PedroCayo

--- src/assets/components/SearchBar.jsx
+++ src/assets/components/SearchBar.jsx
@@ -1,5 +1,43 @@
 //Buscar un producto: mediante una barra de búsqueda (por descripción o ID).
 //MAXIMILIANO
+<<<<<<< Updated upstream
+=======
+import React, { useState, useCallback } from 'react';
+
+const SearchBar = React.memo(function SearchBar({ onSearch }) {
+  const [termino, setTermino] = useState('');
+
+  const handleSubmit = useCallback((e) => {
+    e.preventDefault();
+    onSearch(termino.trim());
+    setTermino('');
+  }, [termino, onSearch]);
+
+  const handleChange = useCallback((e) => {
+    setTermino(e.target.value);
+  }, []);
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h2>Buscar producto por ID o Nombre:</h2>
+        <label htmlFor="busqueda">ID o Nombre:</label>
+        <input
+          id="busqueda"
+          type="text"
+          value={termino}
+          onChange={handleChange}
+          placeholder="Ej: 101 o 'camisa'"
+        />
+        <div>
+          <button type="submit" className="boton">Buscar</button>
+        </div>
+      </form>
+    </div>
+  );
+});
+
+export default SearchBar;
 
 
 
---
+++ src/assets/components/SearchResults.jsx
@@ -0,0 +1,32 @@
+import React, { useMemo } from "react";
+import SearchBar from "./SearchBar";
+
+function SearchResults({ buscarProducto, resultados }) {
+  const renderedResults = useMemo(() => {
+    if (resultados.length === 0) return null;
+
+    return (
+      <section>
+        <h3 className="titulo">Resultados de Búsqueda</h3>
+        <ul>
+          {resultados.map((producto) => (
+            <li key={producto.id}>
+              ID: {producto.id} - {producto.nombre} - Marca: {producto.marca} - Precio: ${producto.precioUnitario} - 
+              Descuento: {producto.descuento}% - Precio Final: ${producto.precioConDescuento.toFixed(2)} - 
+              Stock: {producto.stock} - Estado: {producto.estado ? "Activo" : "Inactivo"}
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
+  }, [resultados]);
+
+  return (
+    <div className="formulario">
+      <SearchBar onSearch={buscarProducto} />
+      {renderedResults}
+    </div>
+  );
+}
+
+export default SearchResults;
